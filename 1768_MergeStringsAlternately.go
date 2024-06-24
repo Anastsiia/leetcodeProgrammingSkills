4353f69847e27779c0c8e3d53672477c18a7f3ba
AnastasiiaPimkin <pimkinaanastasiia@gmail.com>
2024-06-24 11:22:31 +0300

    added 1768 & 389 tasks

---
+++ 1768_MergeStringsAlternately.go
@@ -0,0 +1,27 @@
+/* You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string. Return the merged string.*/
+package main
+
+import "fmt"
+
+func mergeAlternately(word1 string, word2 string) string {
+	var res string
+	maxLen := len(word1)
+	if len(word1) < len(word2) {
+		maxLen = len(word2)
+	}
+	for i := 0; i < maxLen; i++ {
+		if i < len(word1) {
+			res = res + string(word1[i])
+		}
+		if i < len(word2) {
+			res = res + string(word2[i])
+		}
+	}
+	return res
+}
+
+func main() {
+	word1 := "a b c"
+	word2 := "xyz"
+	fmt.Println(mergeAlternately(word1, word2))
+}

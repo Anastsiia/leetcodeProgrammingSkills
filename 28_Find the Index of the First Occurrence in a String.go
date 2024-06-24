68a3c320634c9ed665bdfdf48c0c028690f76fbc
AnastasiiaPimkin <pimkinaanastasiia@gmail.com>
2024-06-24 11:44:53 +0300

    added 28

---
+++ 28_Find the Index of the First Occurrence in a String.go
@@ -0,0 +1,54 @@
+/* Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.*/
+package main
+
+import (
+	"fmt"
+)
+
+/* we can use slices to make in more elegant:
+func strStr(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+	h := len(haystack)
+	n := len(needle)
+	for i := 0; i < h-n+1; i++ {
+		if haystack[i:i+n] == needle {
+			return i
+		}
+	}
+	return -1
+}*/
+
+/* also we can use func string.Index:
+func strStrSample(haystack string, needle string) int {
+	return strings.Index(haystack, needle)
+}*/
+
+func strStr(haystack string, needle string) int {
+	if len(needle) > len(haystack) {
+		return -1
+	}
+	for a, v := range haystack {
+		if v == rune(needle[0]) && len(haystack)-a >= len(needle) {
+			i, b := 0, a
+			for ; i < len(needle); i++ {
+				if haystack[b] == needle[i] {
+					b++
+				} else {
+					break
+				}
+			}
+			if i-1 == len(needle)-1 {
+				return a
+			}
+		}
+	}
+	return -1
+}
+
+func main() {
+	word1 := "a"
+	word2 := "a"
+	fmt.Println(strStr(word1, word2))
+}

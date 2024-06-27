416de70b98019ca5c29fcd66edd4ebd99e33867a
AnastasiiaPimkin <pimkinaanastasiia@gmail.com>
2024-06-27 13:09:57 +0300

    added 1822

---
+++ 1822_Sign of the Product of an Array.go
@@ -0,0 +1,31 @@
+/*
+	There is a function signFunc(x) that returns:
+
+1 if x is positive.
+-1 if x is negative.
+0 if x is equal to 0.
+You are given an integer array nums. Let product be the product of all values in the array nums.
+
+Return signFunc(product).
+*/
+package main
+
+import "fmt"
+
+func arraySign(nums []int) int {
+	res := 1
+	for _, v := range nums {
+		if v == 0 {
+			return 0
+		}
+		if v < 0 {
+			res *= -1
+		}
+	}
+	return res
+}
+
+func main() {
+	nums := []int{-1, 0, 1}
+	fmt.Println(arraySign(nums))
+}

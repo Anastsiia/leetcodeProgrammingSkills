28e4e05d19173fc42afdcebca5bfba57d4e2ff92
AnastasiiaPimkin <pimkinaanastasiia@gmail.com>
2024-07-02 17:36:54 +0300

    1491 added second sorting opportunity

--- 1491_Average Salary Excluding the Minimum and Maximum Salary.go
+++ 1491_Average Salary Excluding the Minimum and Maximum Salary.go
@@ -9,8 +9,11 @@ import "golang.org/x/exp/slices"
 
 func average(salary []int) float64 {
 	res := 0.0
+
+	// slices.Sort - is better to use when you need to sort different type of slice(strings, float)
 	slices.Sort(salary)
-	//also can use
+
+	//also can use. is better because From the standard sort package in the core library.is preferred when you specifically need to sort integer
 	// sort.Ints(salary)
 	for i := 1; i < len(salary)-1; i++ {
 		res += float64(salary[i])
